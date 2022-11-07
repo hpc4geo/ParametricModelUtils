da3c7ffb43b8aa6563055755190dfa31dcda6074
Dave May <dave.mayhem23@gmail.com>
2022-11-07 10:25:32 -0800

    Improve setup of pyscript demo

--- examples/forward_models/forward_pyscript.py
+++ examples/forward_models/forward_pyscript.py
@@ -4,7 +4,7 @@ from parametric_model_utils import ParametricInput
 from parametric_model_utils import parametric_model as pm
 from parametric_model_utils import ExecutionStatus as status
 
-import os
+import os, shutil
 import numpy as np
 import subprocess
 
@@ -18,8 +18,14 @@ class PyscriptModel(pm.ParametricModel):
                           {"name": 'b', "bounds":[0.0, 2.0], "units":'-'},
                           {"name": 'c', "bounds":[-10.0, 1.0], "units":'-'},
                         ] )
-    P.write_definition()
-    os.rename("parametric_def.csv", "pyscript_parametric_def.csv")
+                        
+    opath = os.path.join('./', "tmp-PyscriptModel-def")
+    os.makedirs(opath, exist_ok=True)
+    P.write_definition(output_path=opath)
+    #os.rename("parametric_def.csv", "pyscript_parametric_def.csv")
+    infile = os.path.join('./', "tmp-PyscriptModel-def", "parametric_def.csv")
+    shutil.copy(infile, "pyscript_parametric_def.csv")
+    shutil.rmtree(opath)    
     super().__init__("pyscript_parametric_def.csv", **kwargs)
   
 
