dfe565baf74b0867cc30c273cd28d7d46c1d8c6b
Dave May <dave.mayhem23@gmail.com>
2022-11-07 19:21:02 -0800

    Added stdout, sleep and potentially an error to check capturing of stdout, stderr and return code.

--- examples/forward_models/sum_numbers.py
+++ examples/forward_models/sum_numbers.py
@@ -1,11 +1,19 @@
 
-import sys
+import sys, time
 import numpy as np
 
 if __name__ == "__main__":
+  with open('sum_numbers.start', 'w') as fp:
+    fp.write('')
+  time.sleep(20.0)
   args = sys.argv[1:]
   nums = np.array(args, dtype=np.float64)
+  print('x', nums)
   val = np.sum(nums)
   with open('obs.txt', 'w') as fp:
     fp.write(str(val))
+
+
+  #a = None
+  #a += 1
 
